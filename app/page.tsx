384b05aa7e4c0d643af9e594ce461adcf8cb28cb
Ibrahim El Khansa <itzbahb@gmail.com>
2024-12-25 01:08:13 +0200

    updated title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="main">
-      <h1 className="main__title">H.A.D.I (Health and Disease Identification)</h1>
+      <h1 className="main__title">H.A.D.I</h1>
+      <h1 className="main__sub-title">Health and Disease Identification</h1>
+      <h1 className="main__sub-title">A project by Ibrahim El Khansa</h1>
       <div className="main__image-container">
         <Image src="/qr.png" alt="QR Code" width={200} height={200} priority />
       </div>
